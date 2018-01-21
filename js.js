98acf8e6cf9be61e6339a83e9c0e02168ff21310
Jacques Felicitas Mamaran <34290152+mamaran@users.noreply.github.com>
2018-01-21 16:48:58 +0800

    first commit

---
+++ js.js
@@ -0,0 +1,36 @@
+function results(){
+	 
+  var col = parseInt(document.getElementById("col").value);
+  console.log(col);
+  var row = parseInt(document.getElementById("row").value);
+  console.log(row);
+  
+  
+  var htmlContainer = "<table border='1' id='tab'>";
+  
+  for (var i = 1; i <= row; i++){ 
+    htmlContainer += "<tr>";
+    for (var x = 1; x <= col; x++) {
+      htmlContainer += "<td id='tab1'>"+ (i*x) + "</td>";
+    }
+    
+    htmlContainer += "</tr>";
+  }
+  
+  htmlContainer += "</table>";
+  
+  document.getElementById("multiTable").innerHTML = htmlContainer;
+}
+
+function bgcolor(){
+	var color=document.getElementById("colorbg").value;
+
+	document.getElementById("tab").style.backgroundColor = color;
+}
+
+function tcolor(){
+	var color=document.getElementById("colort").value;
+
+	document.getElementById("tab").style.color = color;
+}
+
